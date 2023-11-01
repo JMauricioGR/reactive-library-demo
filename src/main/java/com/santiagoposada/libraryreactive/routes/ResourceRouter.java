1f86d385d6703d60c3e77bcb5e518594626e5517
JEHISON MAURICIO GALARZA RODRÍGUEZ <jehison.galarza@sofka.com.co>
2023-11-01 15:44:38 -0500

    Mongodb conection string modified to personal mongo atlas database

--- src/main/java/com/santiagoposada/libraryreactive/routes/ResourceRouter.java
+++ src/main/java/com/santiagoposada/libraryreactive/routes/ResourceRouter.java
@@ -35,7 +35,7 @@ public class ResourceRouter {
 
     @Bean
     public RouterFunction<ServerResponse> getAllRouter(GetAllUseCase getAllUseCase){
-        return route(GET("/resources"),
+        return route(GET("user.name"),
                 request -> ServerResponse.ok()
                         .contentType(MediaType.APPLICATION_JSON)
                         .body(BodyInserters.fromPublisher(getAllUseCase.get(), ResourceDTO.class)));
