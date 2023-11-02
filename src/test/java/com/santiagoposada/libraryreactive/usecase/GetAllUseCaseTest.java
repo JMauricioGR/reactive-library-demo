0cc4c1e3af3d70f17deb62308e31588d240d3228
JEHISON MAURICIO GALARZA RODRÍGUEZ <jehison.galarza@sofka.com.co>
2023-11-02 13:54:13 -0500

    Get all resources use case test created successfully with stepverifier

--- src/test/java/com/santiagoposada/libraryreactive/usecase/GetAllUseCaseTest.java
+++ src/test/java/com/santiagoposada/libraryreactive/usecase/GetAllUseCaseTest.java
@@ -1,9 +1,18 @@
 package com.santiagoposada.libraryreactive.usecase;
 
+import com.santiagoposada.libraryreactive.dto.ResourceDTO;
+import com.santiagoposada.libraryreactive.entity.Resource;
 import com.santiagoposada.libraryreactive.repository.ResourceRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 public class GetAllUseCaseTest {
@@ -13,5 +22,67 @@ public class GetAllUseCaseTest {
 
     @SpyBean
     private GetAllUseCase getAllUseCase;
+
+    @Test
+    @DisplayName("Get all resources")
+    void getAllResourcesTest(){
+
+        //Arrange
+        Resource resource = new Resource();
+
+        resource.setId("aaa111bbb");
+        resource.setName("Evolution Theory");
+        resource.setCategory("Biological");
+        resource.setType("Type 1");
+        resource.setLastBorrow(LocalDate.parse("2023-11-02"));
+        resource.setUnitsOwed(2);
+        resource.setUnitsAvailable(8);
+
+        Resource resource2 = new Resource();
+
+        resource2.setId("ccc222ddd");
+        resource2.setName("Cinematic");
+        resource2.setCategory("Physics");
+        resource2.setType("Type 2");
+        resource2.setLastBorrow(LocalDate.parse("2023-11-02"));
+        resource2.setUnitsOwed(1);
+        resource2.setUnitsAvailable(9);
+
+        ResourceDTO resourceDTO = new ResourceDTO();
+
+        resourceDTO.setId("aaa111bbb");
+        resourceDTO.setName("Evolution Theory");
+        resourceDTO.setCategory("Biological");
+        resourceDTO.setType("Type 1");
+        resourceDTO.setLastBorrow(LocalDate.parse("2023-11-02"));
+        resourceDTO.setUnitsOwed(2);
+        resourceDTO.setUnitsAvailable(8);
+
+        ResourceDTO resource2DTO = new ResourceDTO();
+
+        resource2DTO.setId("ccc222ddd");
+        resource2DTO.setName("Cinematic");
+        resource2DTO.setCategory("Physics");
+        resource2DTO.setType("Type 2");
+        resource2DTO.setLastBorrow(LocalDate.parse("2023-11-02"));
+        resource2DTO.setUnitsOwed(1);
+        resource2DTO.setUnitsAvailable(9);
+
+        Flux<Resource> resourceResponse = Flux.just(resource, resource2);
+
+        // Act
+        Mockito.when(resourceRepository.findAll()).thenReturn(resourceResponse);
+
+        // Assert
+
+        Flux<ResourceDTO> result = getAllUseCase.get();
+
+        StepVerifier.create(result)
+                .expectNext(resourceDTO)
+                .expectNext(resource2DTO)
+                .expectComplete()
+                .verify();
+
+    }
 
 }
